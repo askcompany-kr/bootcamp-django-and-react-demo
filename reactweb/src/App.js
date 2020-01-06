995640142130a7155fea0d4ca4f6d0611826dd2d
Chinseok Lee <me@askcompany.kr>
2020-01-06 13:53:48 +0900

    PostList 컴포넌트 추가

--- reactweb/src/App.js
+++ reactweb/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.scss';
+import PostList from "./pages/PostList";
 
 function App() {
   return (
     <div className="App">
+      <h1>Bootstap Django Demo</h1>
+      <PostList />
     </div>
   );
 }
---
+++ reactweb/src/pages/PostList.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./PostList.module.scss";
+
+
+export default function PostList() {
+  return (
+    <div className={styles.post_list}>
+      PostList with React
+    </div>
+  );
+}
