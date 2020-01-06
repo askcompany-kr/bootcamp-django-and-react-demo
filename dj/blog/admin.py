193eeae855cd171e2514e7d1d2106f02911b3971
Chinseok Lee <me@askcompany.kr>
2020-01-06 11:16:06 +0900

    blog PostAdmin 추가

--- dj/blog/admin.py
+++ dj/blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'message', 'updated_at']
+    list_display_links = ['message']
+    search_fields = ['message']
+    list_filter = ['updated_at']
