d5e050ec9826506d2568a4a88851c947fb6df516
Chinseok Lee <me@askcompany.kr>
2020-01-20 17:50:20 +0900

    redux-devtools-extension 활용하기 주석 추가

--- reactweb/src/contexts/ReduxProvider.js
+++ reactweb/src/contexts/ReduxProvider.js
@@ -4,6 +4,7 @@ import rootReducer from "../store/modules";
 import {Provider} from "react-redux";
 
 
+// FIXME: redux-devtools-extension 활용하기
 const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
 const store = createStore(rootReducer, devToolsExtension);
