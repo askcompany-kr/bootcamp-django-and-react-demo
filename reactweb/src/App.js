db306893e6ad28f6288f25f52d1d68cf720d9388
Chinseok Lee <me@askcompany.kr>
2020-01-20 14:48:42 +0900

    jwtToken 출력 제거

--- reactweb/src/App.js
+++ reactweb/src/App.js
@@ -128,8 +128,6 @@ function App() {
         </Layout.Content>
         <Layout.Footer style={{ textAlign: 'center' }}>
           Ant Design ©2018 Created by Ant UED
-          <hr/>
-          {jwtToken}
         </Layout.Footer>
       </Layout>
     </BrowserRouter>
